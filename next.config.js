9f6f798be88001fb02c6605f9f3b39d4064c7219
Rukeith Tseng <rukeith.tseng@shopee.com>
2020-01-16 10:03:00 +0800

    Update next to 9.2.0
    Signed-off-by: Rukeith Tseng <rukeith.tseng@shopee.com>

--- next.config.js
+++ next.config.js
@@ -5,33 +5,30 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 const withSourceMaps = require('@zeit/next-source-maps');
 
 module.exports = withBundleAnalyzer(
   withSourceMaps(
-    withCSS(
-      withSass({
-        webpack(config, options) {
-          config.plugins = config.plugins || [];
+    withSass({
+      webpack(config, options) {
+        config.plugins = config.plugins || [];
 
-          config.plugins = [
-            ...config.plugins,
+        config.plugins = [
+          ...config.plugins,
 
-            new Dotenv({
-              path: path.join(__dirname, '.env'),
-              systemvars: true
-            })
-          ];
-          return config;
-        },
-        sassLoaderOptions: {
-          sourceMap: true
-        },
-        postcssLoaderOptions: {
-          sourceMap: true
-        }
-      })
-    )
+          new Dotenv({
+            path: path.join(__dirname, '.env'),
+            systemvars: true
+          })
+        ];
+        return config;
+      },
+      sassLoaderOptions: {
+        sourceMap: true
+      },
+      postcssLoaderOptions: {
+        sourceMap: true
+      }
+    })
   )
 );
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import 'normalize.css';
 
 export default class MyDocument extends Document {
   render() {
