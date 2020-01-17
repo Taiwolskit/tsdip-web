5dff976932246f482b6d7bb73763a5da629c86e2
Rukeith Tseng <rukeith.tseng@shopee.com>
2020-01-17 15:34:10 +0800

    Rollback
    Signed-off-by: Rukeith Tseng <rukeith.tseng@shopee.com>

--- next.config.js
+++ next.config.js
@@ -5,30 +5,35 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
 const withSourceMaps = require('@zeit/next-source-maps');
 
 module.exports = withBundleAnalyzer(
   withSourceMaps(
-    withSass({
-      webpack(config, options) {
-        config.plugins = config.plugins || [];
+    withCSS(
+      withSass({
+        webpack(config, options) {
+          config.plugins = config.plugins || [];
 
-        config.plugins = [
-          ...config.plugins,
-
-          new Dotenv({
-            path: path.join(__dirname, '.env'),
-            systemvars: true
-          })
-        ];
-        return config;
-      },
-      sassLoaderOptions: {
-        sourceMap: true
-      },
-      postcssLoaderOptions: {
-        sourceMap: true
-      }
-    })
+          if (process.env.NODE_ENV !== 'production') {
+            config.plugins = [
+              ...config.plugins,
+    
+              new Dotenv({
+                path: path.join(__dirname, '.env'),
+                systemvars: true
+              })
+            ];
+          }
+          return config;
+        },
+        sassLoaderOptions: {
+          sourceMap: true
+        },
+        postcssLoaderOptions: {
+          sourceMap: true
+        }
+      })
+    )
   )
 );
