489d03673b385fabfad86738fcc35d7ca8f82d3b
Edwin Jarvis <fenglan.crazydance@gmail.com>
2020-03-22 13:23:52 +0800

    Update Admin Dashboard
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

---
+++ components/Temp/SidebarItem/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import styles from './SidebarItem.module.scss';
+
+const SidebarItem = ({ caption, url }) => (
+  <Link href={url}>
+    <li className={styles['dashboard-page-sidebar-item']}>
+      <a href={url}>{caption}</a>
+    </li>
+  </Link>
+);
+
+SidebarItem.propTypes = {
+  caption: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+};
+
+export default SidebarItem;
