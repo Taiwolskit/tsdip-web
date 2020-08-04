51f15e720f23a8100bb3ea31f03ddac343651bd0
Rukeith Tseng <fenglan.crazydance@gmail.com>
2020-08-04 14:05:44 +0800

    Add storybook theme

--- stories/header.stories.js
+++ stories/header.stories.js
@@ -2,8 +2,18 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default {
-  title: 'Button',
+  title: 'Header',
   component: Header,
 };
 
 export const HeaderNav = () => <Header />;
+
+HeaderNav.story = {
+  name: 'Header Navs',
+};
+
+export const HeaderNavss = () => <Header />;
+
+HeaderNavss.story = {
+  name: 'Header Navsvvv',
+};
