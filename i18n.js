bead903e1e7a95e0ea184f26b07896387dddf156
Edwin Jarvis <fenglan.crazydance@gmail.com>
2020-08-05 22:37:44 +0800

    update i18n setting
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

--- i18n.js
+++ i18n.js
@@ -1,10 +1,8 @@
-const NextI18Next = require('next-i18next').default;
-// const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 const path = require('path');
+const NextI18Next = require('next-i18next').default;
 
 module.exports = new NextI18Next({
-    // browserLanguageDetection: true,
+  fallbackLng: 'en',
   otherLanguages: ['zh-TW'],
-//   localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
 });
