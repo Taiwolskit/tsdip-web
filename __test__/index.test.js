7192313460817af22fef72f34f79bf047c5c54da
Rukeith Tseng <fenglan.crazydance@gmail.com>
2020-08-14 14:01:47 +0800

    Fix jest
    Signed-off-by: Rukeith Tseng <fenglan.crazydance@gmail.com>

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { I18nextProvider } from 'react-i18next';
 import IndexPage from '../pages/index';
+import Header from '../components/Header';
+import i18n from '../i18n';
 
 describe('Pages', () => {
     describe('Index', () => {
         test('should render without throwing an error', () => {
             const wrap = mount(<IndexPage />);
-            expect(wrap.find('button').text()).toBe('Hello Next.js');
+            // expect(wrap.find('button').text()).toBe('Hello Next.js');
+            expect(wrap.contains(<Header />)).to.equal(true);
         });
 
         test('should render without throwing an error', () => {
