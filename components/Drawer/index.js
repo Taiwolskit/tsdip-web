70ff1928612a29a603958231d4c6262701fcca90
Rukeith Tseng <fenglan.crazydance@gmail.com>
2020-08-28 13:33:44 +0800

    Update drawer content
    Signed-off-by: Rukeith Tseng <fenglan.crazydance@gmail.com>

--- components/Drawer/index.js
+++ components/Drawer/index.js
@@ -24,6 +24,10 @@ import PersonIcon from '@material-ui/icons/Person';
 
 import { withTranslation } from '../../i18n';
 
+import Profile from '../Profile';
+import Event from '../Event';
+import Organization from '../Organization';
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -87,6 +91,18 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3),
   },
 }));
+
+const renderSwitch = (param) => {
+  switch (param) {
+    case 1:
+      return <Event />;
+    case 2:
+      return <Organization />;
+    case 0:
+    default:
+      return <Profile />;
+  }
+};
 
 const MiniDrawer = ({ t, sidebarItems }) => {
   const classes = useStyles();
@@ -175,21 +191,7 @@ const MiniDrawer = ({ t, sidebarItems }) => {
 
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
+        {renderSwitch(selectedIndex)}
       </main>
     </div>
   );
