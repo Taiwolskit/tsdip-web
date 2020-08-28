2162f0e9f292410c624f66ad71705376330336b2
Rukeith Tseng <fenglan.crazydance@gmail.com>
2020-08-28 08:41:14 +0800

    Update test
    Signed-off-by: Rukeith Tseng <fenglan.crazydance@gmail.com>

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { mount } from 'enzyme';
 import IndexPage from '../pages/index';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 describe('Pages', () => {
   describe('Index', () => {
-    test('should render without throwing an error', () => {
+    test('Check Header component', () => {
       const wrap = mount(<IndexPage />);
       expect(wrap.contains(<Header />)).toEqual(true);
+    });
+
+    test('Check Footer component', () => {
+      const wrap = mount(<IndexPage />);
+      expect(wrap.contains(<Footer />)).toEqual(true);
     });
   });
 });
